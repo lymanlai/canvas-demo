11012bb49efd6a84ce1842ac117c6d2a61617583
Lyman Lai <lyman.lai@yaha.me>
2015-08-23 13:54:56 +0800

    add base Demo code

--- demo.js
+++ demo.js
@@ -1,6 +1,25 @@
 (function() {
     window.addEventListener("load", Demo.load, false);
     var Demo = {
+        data: {
 
+        },
+        get: get,
+        set: set,
+        load: load
     };
+
+    function get(key) {
+        return Demo.data[key] || null;
+    }
+
+    function set(key, val) {
+        Demo.data[key] = val;
+
+        return Demo;
+    }
+
+    function load() {
+
+    }
 })();
