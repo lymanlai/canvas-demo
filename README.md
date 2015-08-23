8879f3ffa62b07a1cf0e38de90503209e83da068
Lyman Lai <lyman.lai@yaha.me>
2015-08-23 14:46:44 +0800

    draw the circle

--- README.md
+++ README.md
@@ -2,11 +2,6 @@
 
 # TODO
 
-1. 编程：
-1). 请基于WebGL或OpenGL实现以下功能（Android or iPhone or Windows or in Browser or in WeChat都可以）
-  b. 绘制一个边颜色#222222、填充颜色#555555的2D圆，命名为圆A
-  c. 圆的大小为canvas的一半
-  c. 使该圆相对于canvas居中
 2). 在1).的基础上实现以下要求：
   a. 实现可以用鼠标或手指拖拽该圆
   b. 在canvas背景层与圆A层之间绘制一个500x500相对于canvas居中颜色为#AAAAAA的填充正方形B
--- demo.js
+++ demo.js
@@ -1,12 +1,17 @@
 (function() {
-    window.addEventListener("load", Demo.load, false);
-    var Demo = {
-        data: {
+    window.addEventListener('load', load, false);
 
-        },
+    function load() {
+        Demo.init();
+        Demo.makeCircleA();
+    }
+
+    var Demo = {
+        data: {},
         get: get,
         set: set,
-        load: load
+        init: init,
+        makeCircleA: makeCircleA
     };
 
     function get(key) {
@@ -19,7 +24,35 @@
         return Demo;
     }
 
-    function load() {
+    function init() {
+        var canvas = document.getElementById('canvas-frame');
+        Demo.set('canvas', canvas);
+        Demo.set('context', canvas.getContext('2d'));
+        Demo.set('canvasWidth', canvas.width);
 
+        return Demo;
     }
+
+    function makeCircleA() {
+        var canvasWidth = Demo.get('canvasWidth');
+        var x = canvasWidth / 2;
+        var y = canvasWidth / 2;
+        var radius = canvasWidth / 4;
+        var fillStyle = '#555';
+        var strokeStyle = '#222';
+
+        var canvas = Demo.get('canvas');
+        var context = Demo.get('context');
+
+        context.fillStyle = fillStyle;
+        context.lineWidth = 1;
+        context.strokeStyle = strokeStyle;
+        context.stroke();
+
+        context.beginPath();
+        context.arc(x, y, radius, 0, 2 * Math.PI, false);
+        context.closePath();
+        context.fill();
+    }
+
 })();
